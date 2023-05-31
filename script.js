b729ecbfa0bf55f4f0799caa14aec4bc9a808272
Matthew Jacaway <matthew.jacaway@student.centralia.edu>
2023-05-31 10:57:26 -0700

    Added Objects

--- script.js
+++ script.js
@@ -1,1 +1,87 @@
-alert("Hello World");
+// index.html Smash Games - Home
+// blast.html Smash Games - Inferno Blast
+
+const data = {
+    brandName: "Smash Games",
+    pages: [
+        {
+            name: "index",
+            pageName: "Home",
+            callToAction: {
+                src: "images/SmashGamesLogo-short.png",
+                altText: "Smash Games Logo"
+
+            }
+        },
+        {
+            name: "blast",
+            pageName: "Inferno Blast",
+            callToAction: {
+                src: "images/Inferno-Jumbotron.png",
+                altText: "Inferno Blast Gameplay",
+                buttonLinkSrc: "https://steampowered.com",
+                buttonLinkText: "Buy now on Steam"
+
+            }
+        }
+
+    ]
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//const data = {
+//     cards:[
+//         {
+//             imgPath: "./somewhere.img",
+//             imgAltText: "some alternate text",
+//             title: "The Title",
+//             body: "Some kind of body of text",
+//             actionLink: "http://localhost:5500/games/game.html"
+//         },
+//         {
+//             imgPath: "./somewhere.img",
+//             imgAltText: "some alternate text",
+//             title: "The Title",
+//             body: "Some kind of body of text",
+//             actionLink: "http://localhost:5500/index.html"
+//         },
+//         {
+//             imgPath: "./somewhere.img",
+//             imgAltText: "some alternate text",
+//             title: "The Title",
+//             body: "Some kind of body of text",
+//             actionLink: "http://localhost:5500/index.html"
+//         },
+//     ]
+// }
+    
+// const anotherData = [
+//     {toast: "bar"},
+//     {toast: "boof"},
+//     {foo: "bar", doStuff: function(){console.log("stuff");}},
+    
+// ]
+    

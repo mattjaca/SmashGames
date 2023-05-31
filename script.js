b5e1e1901ba0ca7c6fcfb35fc0121e6d65a6f5dc
Matthew Jacaway <matthew.jacaway@student.centralia.edu>
2023-05-31 11:55:45 -0700

    added more data

--- script.js
+++ script.js
@@ -10,23 +10,48 @@ const data = {
             callToAction: {
                 src: "images/SmashGamesLogo-short.png",
                 altText: "Smash Games Logo"
-
             }
         },
         {
             name: "blast",
             pageName: "Inferno Blast",
-            callToAction: {
-                src: "images/Inferno-Jumbotron.png",
-                altText: "Inferno Blast Gameplay",
-                buttonLinkSrc: "https://steampowered.com",
-                buttonLinkText: "Buy now on Steam"
-
-            }
+            blocks: [
+                {
+                    type: "call-to-action",
+                    src: "images/Inferno-Jumbotron.png",
+                    altText: "Inferno Blast Gameplay",
+                    buttonLinkSrc: "https://steampowered.com",
+                    buttonLinkText: "Buy now on Steam"
+                },
+                {
+                    type: "description",
+                    text: "Inferno Blast is an action-packed adventure game that takes players to a fiery realm of danger and excitement. In this game, you'll take on the role of a hero who must navigate through a series of treacherous levels filled with fiery obstacles and hordes of demons. With each level you complete, you'll unlock new weapons and power-ups to help you in your quest to defeat the ultimate demon boss. Can you survive the inferno and emerge victorious?"
+                },
+                {
+                    type: "deck",
+                    cards: [
+                        {
+                            type: "card",
+                            src: "images/Inferno/Inferno-2-short.png",
+                            altText: "Inferno Blast Gameplay",
+                            title: "Dynamic Environments",
+                            body: "Navigate through a variety of fiery landscapes, including lava pits, crumbling ruins, and demonic strongholds."
+                        },
+                        {
+                            type: "card",
+                            src: "images/Inferno/Inferno-1-short.png",
+                            altText: "Inferno Blast Gameplay",
+                            title: "Upgradable Weapons",
+                            body: "Customize your arsenal with a range of weapons and power-ups, including flamethrowers, rocket launchers, and more."
+                        }
+                    ],
+                }
+            ],
         }
-
     ]
 }
+
+console.log(data.pages[1].blocks[2]);
 
 
 
